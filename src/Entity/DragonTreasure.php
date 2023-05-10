e1e974d7b5c89c2a2361ae2e2442b7da00380fd6
Yuchuan <yuchuan@example.com>
2023-05-10 18:24:56 -0400

    ch26 - Filtering on Relations

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -59,6 +59,7 @@ use function Symfony\Component\String\u;
 )]
 // #[ApiFilter(BooleanFilter::class, properties: ['isPublished'])]
 #[ApiFilter(PropertyFilter::class)]
+#[ApiFilter(SearchFilter::class, properties: ['owner.username' => 'partial'])]
 class DragonTreasure
 {
     #[ORM\Id]
@@ -105,6 +106,7 @@ class DragonTreasure
     #[ORM\JoinColumn(nullable: false)]
     #[Groups(['treasure:read', 'treasure:write'])]
     #[Assert\Valid]
+    #[ApiFilter(SearchFilter::class, strategy: 'exact')]
     private ?User $owner = null;
 
     public function __construct(string $name = null)
